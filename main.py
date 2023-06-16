5617a122ebbb9a866cdbac0f23622009be86853e
Nirmala Joshi <nirmalasjoshis@gmail.com>
2023-06-16 08:31:02 -0400

    Adding pycharm main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+import random
+#instance of FasctAPI class
+app = FastAPI()
+#Function to reads facts file and display randomly
+def read_facts():
+    with open('facts.txt', 'r') as file:
+        facts = [line.strip() for line in file]
+    return facts
+facts_list = read_facts()
+#Function to displayfacts fom  facts.txt file randomly
+@app.get('/facts')
+def read_random_fact():
+    random_fact = random.choice(facts_list)
+    return{'fact': random_fact}
+
